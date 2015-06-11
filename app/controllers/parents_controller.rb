2644fbf6781ace47f2a4d5f03b86e071f5f36df5
John M Buscemi <jmbuscemi@gmail.com>
2015-06-11 08:36:54 -0400

    Regain sanity. Create login and index page for parents. #shipit

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,75 +1,46 @@
 class ParentsController < ApplicationController
-  before_action :logged_in?
-  before_action :set_student, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in?, except: [:new, :create]
+  before_action :set_parent, only: [:show, :edit, :update, :destroy]
 
-  # GET /parents
-  # GET /parents.json
-  def index
-    @parents = Parent.all
-  end
-
-  # GET /parents/1
-  # GET /parents/1.json
-  def show
-  end
-
-  # GET /parents/new
   def new
-    @parent = Parent.new
+    if session[:teacher_id]
+      @parent = Parent.new
+    else
+      flash.now[:alert] = "You can't access this page"
+    end
   end
 
-  # GET /parents/1/edit
-  def edit
-  end
-
-  # POST /parents
-  # POST /parents.json
   def create
-    @parent = Parent.new(student_params)
-
-    respond_to do |format|
-      if @parent.save
-        format.html { redirect_to @parent, notice: 'Parent was successfully created.' }
-        format.json { render :show, status: :created, location: @parent }
-      else
-        format.html { render :new }
-        format.json { render json: @parent.errors, status: :unprocessable_entity }
-      end
+    @parent = Parent.new(parent_params)
+    @parent.teacher_id = session[:teacher_id]
+    if @parent.save
+      redirect_to teachers_path, notice: 'Parent was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /parents/1
-  # PATCH/PUT /parents/1.json
-  def update
-    respond_to do |format|
-      if @parent.update(student_params)
-        format.html { redirect_to @parent, notice: 'Parent was successfully updated.' }
-        format.json { render :show, status: :ok, location: @parent }
-      else
-        format.html { render :edit }
-        format.json { render json: @parent.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /parents/1
-  # DELETE /parents/1.json
-  def destroy
-    @parent.destroy
-    respond_to do |format|
-      format.html { redirect_to parents_url, notice: 'Parent was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  def index
+    @parent = Parent.find_by_id(session[:parent_id])
+    @kid = @parent.student 
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_student
-      @parent = Parent.find(params[:id])
+    def set_parent
+      @parent = Parent.find(session[:parent_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def student_params
-      params.require(:parent).permit(:name, :email, :password_digest, :teacher_id)
+    def parent_params
+      params.require(:parent).permit(:name, :email, :password, :teacher_id, :password_confirmation)
+    end
+
+    def logged_in?
+      if session[:user_type] == "student" || session[:user_type] == "teacher"
+        redirect_to login_path, notice: "You can't view that page"
+      else
+        true
+      end
     end
 end
